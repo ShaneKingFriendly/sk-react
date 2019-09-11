32081b30ff7f9dd1c8641a45969d490967ca3c4e
ShaneKing <github@azolla.org>
2019-09-12 00:33:11 +0800

    upgrade dep

--- src/sk/index.js
+++ src/sk/index.js
@@ -1,5 +1,4 @@
 export SKCountUp from './SKCountUp';
 export SKECharts from './SKECharts';
-export SKGoJSDag from './SKGoJSDag';
 export SKJdenticon from './SKJdenticon';
 export SKSqlCodeMirror from './SKSqlCodeMirror';
